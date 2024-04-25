9a9874ca5f36ad85e58498e9bcb832f6c9af6e94
lujaja <jarzcyber@gmail.com>
2024-04-25 21:51:46 +0300

    js classes

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,29 @@
+/* eslint-disable no-underscore-dangle */
+class Airport {
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    this._name = value;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    this._code = value;
+  }
+
+  toString() {
+    return `[object ${this._code}]`;
+  }
+}
+
+export default Airport;

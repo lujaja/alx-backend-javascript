9c334afa53a6d638576c201b9021dae2b5517a2f
lujaja <jarzcyber@gmail.com>
2024-04-25 17:56:31 +0300

    classes

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -6,9 +6,10 @@ import ClassRoom from './0-classroom';
  */
 
 export default function initializeRooms() {
-  return [
+  const newList = [
     new ClassRoom(19),
     new ClassRoom(20),
     new ClassRoom(34),
   ];
-}
+  return (newList);
+};

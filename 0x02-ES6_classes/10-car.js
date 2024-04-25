f90ee70c734041fb0d03dd1868a58edf124bf762
lujaja <jarzcyber@gmail.com>
2024-04-25 23:08:25 +0300

    js classes

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,19 @@
+const cloneSymbol = Symbol('cloneCar');
+
+class Car {
+    constructor(brand, motor, color) {
+        this._brand = brand;
+        this._motor = motor;
+        this._color = color;
+    }
+
+    [cloneSymbol]() {
+        return new Car(this._brand, this._motor, this._color);
+    }
+
+    cloneCar() {
+        return this[cloneSymbol]();
+    }
+}
+
+export default Car;
--- 0x02-ES6_classes/2-main.js
+++ 0x02-ES6_classes/2-main.js
@@ -1,5 +1,7 @@
-import HolbertonClass from "./8-hbtn_class.js";
+import EVCar from './100-evcar.js';
 
-const hc = new HolbertonClass(12, "Mezzanine")
-console.log(Number(hc));
-console.log(String(hc));
+const ec1 = new EVCar("Tesla", "Turbo", "Red", "250");
+console.log(ec1);
+
+const ec2 = ec1.cloneCar();
+console.log(ec2);

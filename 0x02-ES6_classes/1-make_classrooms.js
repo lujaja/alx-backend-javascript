b4a1388bdaaf25824ff1ff97bf28d0b6e114cd8b
lujaja <jarzcyber@gmail.com>
2024-04-25 16:02:28 +0300

    es6 classes

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,14 @@
+import ClassRoom from './0-classroom.js';
+
+/**
+ * Initialize an array of ClassRoom objects with specified sizes.
+ * @Return [Array} Array of ClassRoom objects.
+ */
+
+export default function initializeRooms () {
+  return [
+    new ClassRoom(19),
+    new ClassRoom(20),
+    new ClassRoom(34)
+  ];
+}

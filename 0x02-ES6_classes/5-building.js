4a6eb1615baed649edfd9a3716c63fa87e27710c
lujaja <jarzcyber@gmail.com>
2024-04-25 21:07:23 +0300

    js classes

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,15 +1,17 @@
 /* eslint-disable no-underscore-dangle */
+// 5-building.js
+
 class Building {
   constructor(sqft) {
+    if (new.target !== Building && this.evacuationWarningMessage === undefined) {
+      throw Error('Class extending Building must override evacuationWarningMessage');
+    }
     this._sqft = sqft;
   }
 
+  // Getter for sqft
   get sqft() {
     return this._sqft;
-  }
-
-  evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 }
 

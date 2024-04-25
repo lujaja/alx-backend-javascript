a3591886b56d4646f8577560e4f4e1c76e71842b
lujaja <jarzcyber@gmail.com>
2024-04-25 16:03:46 +0300

    es6 classes

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,6 +1,5 @@
-// class
 export default class ClassRoom {
-  constructor (maxStudentsSize) {
-    this._maxStudentsSize = maxStudentsSize;
-  }
+    constructor(maxStudentsSize) {
+        this._maxStudentsSize = maxStudentsSize;
+    }
 }

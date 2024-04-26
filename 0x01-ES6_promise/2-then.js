bf1fd1e4f576ab889431f20fdc35f28acc139634
lujaja <jarzcyber@gmail.com>
2024-04-26 15:16:16 +0300

    promise

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -3,7 +3,7 @@ function handleResponseFromAPI(promise) {
     console.log('Got a response from the API');
     return { status: 200, body: 'success' };
   }).catch(() => {
-    console.log('Got a response from the API');
+    // console.log('Got a response from the API');
     return (new Error());
   });
 }

2164d7dc1e9b0d2e35b7dd9f541bfe56f6b65a10
lujaja <jarzcyber@gmail.com>
2024-04-26 17:52:34 +0300

    promise

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,4 +1,3 @@
-/*
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
@@ -9,29 +8,6 @@ function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([userp, photp])
     .then((results) => ({ status: 'fulfilled', value: results.value }))
     .catch((error) => ({ status: 'rejected', value: error.reason }));
-}
-
-export default handleProfileSignup;
-*/
-import signUpUser from './4-user-promise';
-import uploadPhoto from './5-photo-reject';
-
-function handleProfileSignup(firstName, lastName, fileName) {
-  const userPromise = signUpUser(firstName, lastName);
-  const photoPromise = uploadPhoto(fileName);
-
-  return Promise.allSettled([userPromise, photoPromise]).then((results) => results.map((result) => {
-    if (result.status === 'fulfilled') {
-      return {
-        status: 'fulfilled',
-        value: result.value,
-      };
-    }
-    return {
-      status: 'rejected',
-      value: result.reason,
-    };
-  }));
 }
 
 export default handleProfileSignup;

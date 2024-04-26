17505461b0b4d75220f31f45048b6d6f20ab9d32
lujaja <jarzcyber@gmail.com>
2024-04-26 18:19:58 +0300

    promise

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,16 @@
+function guardrail(mathFunction) {
+  const queue = [];
+
+  try {
+    const result = mathFunction();
+    queue.push(result);
+  } catch (error) {
+    queue.push(error.toString());
+  } finally {
+    queue.push('Guardrail was processed');
+  }
+
+  return queue;
+}
+
+export default guardrail;

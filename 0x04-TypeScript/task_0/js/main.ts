1c6619baa4092cc07d6c6606fef2654e843725c0
lujaja <jarzcyber@gmail.com>
2024-05-23 14:20:18 +0300

    interface

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,48 @@
+interface Student {
+    firstName: string;
+    lastName: string;
+    age: number;
+    location: string;
+}
+
+const student1: Student = {
+    firstName: "Lujaja",
+    lastName: "Luvuga",
+    age: 27,
+    location: "Eldoret"
+}
+
+const student2: Student = {
+    firstName: "Joan",
+    lastName: "Nelima",
+    age: 25,
+    location: "Eldoret"
+}
+
+const studentsList: Student[] = [student1, student2];
+
+function renderTable() {
+    const table = document.createElement("table");
+    const tbody = document.createElement("tbody");
+
+    studentsList.forEach((student) => {
+        const tr = document.createElement("tr");
+        const tdFirstName = document.createElement("td");
+        const tdLocation = document.createElement("td");
+
+        // Set text content for the table cells
+        tdFirstName.textContent = student.firstName;
+        tdLocation.textContent = student.location;
+
+        tr.appendChild(tdFirstName);
+        tr.appendChild(tdLocation);
+
+        tbody.appendChild(tr);
+    });
+
+    table.appendChild(tbody);
+    document.body.appendChild(table);
+}
+
+renderTable();
+
